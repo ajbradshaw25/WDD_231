0001237121423d7d629aa35b5fc56fa56a181d72
sapphireflowers <sapphireflowers30@gmail.com>
2026-03-11 08:19:25 -0700

    wdd

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,8 @@ import { mediaCardTemplate } from "./templates.mjs";
 
 function setParkIntro(data) {
   const introEl = document.querySelector(".intro");
-  introEl.innerHTML = `<h1>${parkData.fullName}</h1>
-  <p>${parkData.description}</p>`;
+  introEl.innerHTML = `<h1>${data.fullName}</h1>
+  <p>${data.description}</p>`;
 }
 
 function setParkInfoLinks(data) {
